28b0fa0273cb33c9f0fb46306a3d9f08b0810c70
Safaritu <safarialex64@gmail.com>
2026-01-02 14:34:34 +0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,5 @@
+// auth.js
+const supabaseUrl = 'https://hoqenpnkmnsfyqsvfvab.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvcWVucG5rbW5zZnlxc3ZmdmFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5Mzk5MTUsImV4cCI6MjA4MjUxNTkxNX0.qKqv6NEyCU5ZMNj6Z34kpCiY7NoUzzBggiPSRJdTz0Y'; 
+
+export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
