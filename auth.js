def0e0f062db4f9f03a621c280c5ce00552d127d
Safaritu <safarialex64@gmail.com>
2026-01-12 04:56:53 +0300

    Set persistSession to false in auth configuration

--- auth.js
+++ auth.js
@@ -1,10 +1,9 @@
-// auth.js
 const supabaseUrl = 'https://hoqenpnkmnsfyqsvfvab.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvcWVucG5rbW5zZnlxc3ZmdmFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5Mzk5MTUsImV4cCI6MjA4MjUxNTkxNX0.qKqv6NEyCU5ZMNj6Z34kpCiY7NoUzzBggiPSRJdTz0Y';
 
 export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey, {
     auth: {
-        persistSession: true, // CHANGE THIS TO FALSE
+        persistSession: false, // Updated as requested
         autoRefreshToken: true,
         detectSessionInUrl: true 
     }
